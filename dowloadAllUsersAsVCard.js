0f96a8ad9b91c0ee93b62b26d3219adb50c59618
Janosch Schwalm <janosch.schwalm@eb.de>
2021-01-28 12:47:07 +0100

    do not download external users

--- dowloadAllUsersAsVCard.js
+++ dowloadAllUsersAsVCard.js
@@ -72,6 +72,7 @@ function insertButtonAndSelect() {
 
   firmen = allUsers.filter(x => x.profile !== undefined)
     .filter(x => x.profile.firma !== undefined)
+    .filter(x => x.profile.firma !== 'Extern')
     .map(y => y.profile.firma)
   options = new Set([allFirma].concat(firmen));
   options.forEach(opt => {
