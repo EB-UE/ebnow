2b9d3f61f2465140c62658f05688658ba2cef390
ebnow <58810927+ebnow@users.noreply.github.com>
2022-04-08 09:08:23 +0200

    Update sabiosucheintegration.js
    Skript für ergebnislistenWidget angepasst

--- sabiosucheintegration.js
+++ sabiosucheintegration.js
@@ -2,7 +2,7 @@
   let suchWidget='<sw-knowledge-search></sw-knowledge-search>'
   let automatischesSuchWidget='<sw-knowledge-auto-search></sw-knowledge-auto-search>'
   let filterWidget='<sw-knowledge-filter></sw-knowledge-filter>'
-  let ergebnislistenWidget='<sw-knowledge-result-list columns="title"></sw-knowledge-result-list>'
+  let ergebnislistenWidget='<sw-knowledge-result-list view-mode="card" columns="title"></sw-knowledge-result-list>'
   let detailansichtWidget='<sw-knowledge-detail></sw-knowledge-detail>'
   let baumWidget='<sw-knowledge-tree></sw-knowledge-tree>'
 
