7694389343bfb35a22e0583150019b66e42e1066
ebnow <58810927+ebnow@users.noreply.github.com>
2022-04-08 08:27:18 +0200

    Update sabiosucheintegration.js
    Ergebnislistenwidget Codeschnipsel angepasst und im Selektor eingesetzt anstatt der detailansicht

--- sabiosucheintegration.js
+++ sabiosucheintegration.js
@@ -2,13 +2,13 @@
   let suchWidget='<sw-knowledge-search></sw-knowledge-search>'
   let automatischesSuchWidget='<sw-knowledge-auto-search></sw-knowledge-auto-search>'
   let filterWidget='<sw-knowledge-filter></sw-knowledge-filter>'
-  let ergebnislistenWidget='<sw-knowledge-result-list></sw-knowledge-result-list>'
+  let ergebnislistenWidget='<sw-knowledge-result-list columns="title"></sw-knowledge-result-list>'
   let detailansichtWidget='<sw-knowledge-detail></sw-knowledge-detail>'
   let baumWidget='<sw-knowledge-tree></sw-knowledge-tree>'
 
 
   let sabiosucheintegrationSelector = document.querySelector('.external-script-widget[data-widget-id="sabiosucheintegration"]');
-  sabiosucheintegrationSelector.innerHTML = `${suchWidget}${detailansichtWidget}${baumWidget}`
+  sabiosucheintegrationSelector.innerHTML = `${suchWidget}${ergebnislistenWidget}${baumWidget}`
 
   
   let sabiostyle = '<style>.button,button{ width:unset; }</style>'
