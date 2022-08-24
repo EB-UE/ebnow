cb11aabf4c4512d76f183c10455225767a104b0d
Janosch Schwalm <janosch.schwalm@gmail.com>
2022-08-24 13:09:10 +0200

    Update sabiosucheintegration.js

--- sabiosucheintegration.js
+++ sabiosucheintegration.js
@@ -1,9 +1,9 @@
 (function() {
-  let suchWidget='<sw-knowledge-search expanded="false" show-path="false"></sw-knowledge-search>'
+  let suchWidget='<sw-knowledge-search></sw-knowledge-search>'
   let automatischesSuchWidget='<sw-knowledge-auto-search></sw-knowledge-auto-search>'
   let filterWidget='<sw-knowledge-filter></sw-knowledge-filter>'
   let ergebnislistenWidget='<sw-knowledge-result-list view-mode="card" columns="title"></sw-knowledge-result-list>'
-  let detailansichtWidget='<sw-knowledge-detail single-expand="true"></sw-knowledge-detail>'
+  let detailansichtWidget='<sw-knowledge-detail expanded="false" show-path="false" single-expand="true"></sw-knowledge-detail>'
   let baumWidget='<sw-knowledge-tree></sw-knowledge-tree>'
 
 
