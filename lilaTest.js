2e563306b52fb75be79bd77e4848c03743305eb5
ebnow <58810927+ebnow@users.noreply.github.com>
2023-01-03 08:29:46 +0100

    Create lilaTest.js

---
+++ lilaTest.js
@@ -0,0 +1,6 @@
+function lila() {
+    const lilaScript = document.createElement('script');
+    lilaScript.src = 'https://stable.loyjoy.com/widget.js?process=81b78f5f-9234-4eaf-b65b-e0dcf566e2bd';
+    document.body.appendChild(lilaScript);
+}
+lila()
