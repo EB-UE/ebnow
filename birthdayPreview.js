6648fb400509f3da45e7b4a9598bd8e8305b8bcb
Janosch Schwalm <janosch.schwalm+github@gmail.com>
2024-09-04 07:18:14 +0200

    Update birthdayPreview.js

--- birthdayPreview.js
+++ birthdayPreview.js
@@ -1,56 +1,54 @@
-(function() {
-  let birthdaySelector = document.querySelector('.external-script-widget[data-widget-id="birthdays"]');
-  birthdaySelector.innerHTML = ""
+await (async function () {
+    const birthdaySelector = document.querySelector('.external-script-widget[data-widget-id="birthdays"]');
+    birthdaySelector.innerHTML = ""
 
-  let dateformat = new Intl.DateTimeFormat('de-DE', {
-      month: '2-digit',
-      day: '2-digit'
-  })
+    const dateformat = new Intl.DateTimeFormat('de-DE', {
+        month: '2-digit',
+        day: '2-digit'
+    })
 
-  function fetchBirthdayAllUser() {
-      we.api.getUsers({
-              limit: 1
-          })
-          .then(response => we.api.getUsers({
-                  limit: response.total
-              })
-              .then(response => {
-                  users =
-                      response.data
-                      .filter(user => user.enabled)
-                      .filter(user => user.status == 'activated')
-                      .filter(user => user.profile)
-                      .filter(user => user.profile.geburtsdatum)
-                  for (let offset = -7; offset <= 30; offset++) {
-                      let d = new Date();
-                      d.setDate(d.getDate() + offset);
-                      displayBirthdays(users, d)
-                  }
-              })
-          );
-  }
+    async function fetchBirthdayAllUser() {
+        const responseToGetMaxUserCount = await we.api.getUsers({
+            limit: 1
+        });
 
-  function displayBirthdays(users, dateToCheck) {
-      let birthdayMessage =
-          users
-          .filter(user => {
-              let birthdayString = user.profile.geburtsdatum
-              let dmy = birthdayString.split(".");
-              let birthday = new Date(dmy[2], dmy[1] - 1, dmy[0]);
-              return dateToCheck.getDate() === birthday.getDate() && dateToCheck.getMonth() === birthday.getMonth();
-          })
-          .map(user => '<a href="/profile/' + user.id + '">' + user.firstName + ' ' + user.lastName + '</a>')
-          .join(", ")
+        const allUsers = await we.api.getUsers({
+            limit: responseToGetMaxUserCount.total
+        })
 
-      let startBold = ""
-      let endBold = ""
-      today = new Date()
-      if (today.getDate() === dateToCheck.getDate() && today.getMonth() === dateToCheck.getMonth()) {
-          startBold = "<strong>"
-          endBold = "</strong>"
-      }
-      birthdaySelector.innerHTML += startBold + dateformat.format(dateToCheck) + ": " + (birthdayMessage || "Keine Geburtstage 😔") + endBold + "<br>";
-  }
+        const relevantUsers =
+            allUsers.data
+                // .filter(user => user.enabled) // no property anymore
+                .filter(user => user.status == 'activated')
+                .filter(user => user.profile?.geburtsdatum)
+        for (let offset = -7; offset <= 30; offset++) {
+            const d = new Date();
+            d.setDate(d.getDate() + offset);
+            displayBirthdays(relevantUsers, d)
+        }
+    }
 
-  fetchBirthdayAllUser()
+    function displayBirthdays(users, dateToCheck) {
+        const birthdayMessage =
+            users
+                .filter(user => {
+                    const birthdayString = user.profile.geburtsdatum
+                    const dmy = birthdayString.split(".");
+                    const birthday = new Date(dmy[2], dmy[1] - 1, dmy[0]);
+                    return dateToCheck.getDate() === birthday.getDate() && dateToCheck.getMonth() === birthday.getMonth();
+                })
+                .map(user => '<a href="/profile/' + user.id + '">' + user.firstName + ' ' + user.lastName + '</a>')
+                .join(", ")
+
+        let startBold = ""
+        let endBold = ""
+        const today = new Date()
+        if (today.getDate() === dateToCheck.getDate() && today.getMonth() === dateToCheck.getMonth()) {
+            startBold = "<strong>"
+            endBold = "</strong>"
+        }
+        birthdaySelector.innerHTML += startBold + dateformat.format(dateToCheck) + ": " + (birthdayMessage || "Keine Geburtstage 😔") + endBold + "<br>";
+    }
+
+    await fetchBirthdayAllUser()
 })();
